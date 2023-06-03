7e555540698b8a2d8bdfdd16a69aa43287987c2a
Roxel 234 <rodrguezruben28@gmail.com>
2023-06-03 14:22:51 -0700

    An Update From Owner

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
-const { readFile } = require("fs");
-
-app.get("/",function(req,res){
-	readFile("./index.html","utf-8",(err,html)=>{
-		if (err) {
-			res.send("Service unavailable");
-		} else {
-			res.send(html);
-		}
-	});
-});
 
 app.listen(port,()=>console.log("Server started on port",port));
