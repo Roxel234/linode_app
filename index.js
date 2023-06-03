a6c3b992f9b37b873d5f5dc3ad1df291f8774a67
Roxel 234 <rodrguezruben28@gmail.com>
2023-06-03 12:53:15 -0700

    yaml file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 80;
+const port = process.env.PORT || 8080;
 const { readFile } = require("fs");
 
 app.get("/",function(req,res){
