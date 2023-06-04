97daa051cd348e22e00f1e780909c7f360a28fd9
Roxel 234 <rodrguezruben28@gmail.com>
2023-06-03 20:35:33 -0700

    An Update From Owner

--- index.js
+++ index.js
@@ -6,4 +6,14 @@ const { readFile } = require("fs");
 
 app.use(express.static(path.join(__dirname,"public")));
 
-app.listen(port,()=>console.log("Server started on port",port));
+const server = app.listen(port,()=>console.log("Server started on port",port));
+
+const socketIO = require("socket.io");
+
+const io = socketIO(server);
+
+io.on("connection",function(socket){
+	console.log("New connection. ID:",socket.id);
+
+	socket.emit("console-log","Your id is '"+socket.id+"'");
+});
