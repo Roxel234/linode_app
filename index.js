d4d4dbb8444b5efce22ae852fae42b7aa77cc30a
Roxel 234 <rodrguezruben28@gmail.com>
2023-06-03 21:04:51 -0700

    An Update From Owner

--- index.js
+++ index.js
@@ -12,8 +12,52 @@ const socketIO = require("socket.io");
 
 const io = socketIO(server);
 
+const rooms = {};
+
+function createRoom(private){
+	let id = 1000 + Math.round(Math.random()*8999);
+
+	rooms["room-"+id] = {
+		canJoin:true,
+		players:[],
+		private
+	};
+
+	console.log("Created room",id,rooms["room-"+id]);
+
+	return id;
+}
+
+function joinRoom(socket,room) {
+	rooms["room-"+room].players.push(socket.id);
+
+	console.log("Socket",socket.id,"joined room",room);
+	socket.emit("console-log","You joined room "+room);
+}
+
+function searchRoom(socket) {
+	let roomKeys = Object.keys(rooms);
+
+	let room2join = undefined;
+
+	for (var i = 0; i < roomKeys.length; i++) {
+		if (rooms[roomKeys[i]].players.length < 4) {
+			room2join = parseInt(roomKeys[i].substring(5),10);
+			break;
+		}
+	}
+
+	if (room2join == undefined) {
+		room2join = createRoom(false);
+	}
+
+	joinRoom(socket,room2join);
+}
+
 io.on("connection",function(socket){
 	console.log("New connection. ID:",socket.id);
+
+	searchRoom(socket);
 
 	socket.emit("console-log","Your id is '"+socket.id+"'");
 });
